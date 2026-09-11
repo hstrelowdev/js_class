cf7d70eda39959b68e00d67684b6b419119bbed3
hans strelow <hstrelowdev@gmail.com>
2026-09-10 21:19:56 -0300

    Leccion basica de objecto

---
+++ 01-tipos/06-objectos.js
@@ -0,0 +1,20 @@
+// personaje de anime
+
+let nombre = "Tanjiro";
+let anime = "demon slayer";
+
+let edad = 16;
+
+let personaje = {
+  nombre: "Tanjiro",
+  anime: "Demon Slayer",
+  edad: 16,
+};
+
+console.log(personaje);
+
+personaje.edad = 13;
+
+personame["edad"] = 16;
+
+delete personaje.anime;
