19687cc6ed139c33a31a76c0488199edbe402798
hans strelow <hstrelowdev@gmail.com>
2026-09-21 22:58:59 -0300

    Actualiza argumentos con comentario

--- 01-tipos/09-argumentos.js
+++ 01-tipos/09-argumentos.js
@@ -6,3 +6,21 @@ function suma(a) {
 let result = suma(1, 2, 3, 4, 5, 6);
 console.log(result);
 console.log(typeof suma);
+
+/*
+declaration
+
+-let
+-const
+- var no es
+-function
+- async function
+- class
+-export / import
+
+
+statements
+
+lo que no esta en el listado anterior
+
+*/
